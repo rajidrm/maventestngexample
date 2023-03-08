274226525c53d6f2818e7822124653e9b61ead3c
HP <HP@192.168.1.46>
2023-03-08 11:51:21 +0530

    First TestNG Maven Project

---
+++ FiirstTestNGMaven/src/test/java/basics/FirstTestNG.java
@@ -0,0 +1,17 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTestNG {
+
+	@Test
+	public void testMyContactForm()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.mycontactform.com/");
+		driver.manage().window().maximize();
+		driver.close();
+	}
+}
